4c1d6053d32c8698c44d5c704bfb049525955dd4
deepanshu-aggarwal <deepanshu.26.da@gmail.com>
2022-06-12 19:28:57 +0530

    initial push

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="w-screen h-auto flex flex-col items-center justify-center">
+      Hello there
+    </div>
+  );
+}
+
+export default App;
