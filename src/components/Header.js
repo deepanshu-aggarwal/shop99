75c5ff4994a46db99ef77216260009e23e11dce6
Raj Rai <rrai1590@gmail.com>
2022-07-03 13:28:08 +0530

    new header Logout icon

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
-import { MdShoppingCart, MdAdd, MdLogout} from "react-icons/md"
+import { MdShoppingCart, MdAdd, MdLogout } from "react-icons/md"
 import Avatar from "./img/avatar.png"
 import { motion } from "framer-motion"
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
@@ -13,19 +13,23 @@ const Header = () => {
     const firebaseAuth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
-    const [{user}, dispatch] = useStateValue()
+    const [{ user }, dispatch] = useStateValue();
+    const [isMenu, setIsMenu] = useState(false);
 
-    const login = async() => {
-        if(!user){
-            const {user: {refreshToken, providerData}} = await signInWithPopup(firebaseAuth, provider)
+    const login = async () => {
+        if (!user) {
+            const { user: { refreshToken, providerData } } = await signInWithPopup(firebaseAuth, provider)
             dispatch({
                 type: actionType.SET_USER,
                 user: providerData[0]
-        })
-        localStorage.setItem('user', JSON.stringify(providerData[0]))
+            })
+            localStorage.setItem('user', JSON.stringify(providerData[0]))
+        }
+        else {
+            setIsMenu(!isMenu);
         }
     }
-    
+
     return (
         <header className=' fixed z-50 w-screen p-6 px-16'>
             {/* desktop */}
@@ -52,21 +56,26 @@ const Header = () => {
                     </motion.div>
 
                     <div className="relative">
-                        <motion.img 
+                        <motion.img
                             whileTap={{ scale: .6 }}
-                            src={user ? user.photoURL : Avatar} 
-                            alt="userProfile" 
+                            src={user ? user.photoURL : Avatar}
+                            alt="userProfile"
                             onClick={login}
-                            className='w-8 min-w-[40] h-8 min-h-[40] cursor-pointer drop-shadow-xl rounded-full' 
+                            className='w-8 min-w-[40] h-8 min-h-[40] cursor-pointer drop-shadow-xl rounded-full'
                         />
-                        <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-10 right-0'>
-                            {user && user.email === "dummygamer18@gmail.com" && (
-                                <Link to='/createItem'>
-                                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
-                                </Link>
-                            )}
-                            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
-                        </div>
+                        {
+                            isMenu && (
+                                <div className='w-40 bg-gray-50 shadow-xl rounded-lg flex flex-col absolute top-10 right-0'>
+                                    {user && user.email === "dummygamer18@gmail.com" && (
+                                        <Link to='/createItem'>
+                                            <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">New Item <MdAdd /></p>
+                                        </Link>
+                                    )}
+                                    <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-slate-100 transition-all duration-100 ease-in-out text-textColor text-base">Logout <MdLogout /></p>
+                                </div>
+                            )
+                        }
+
                     </div>
                 </div>
 
