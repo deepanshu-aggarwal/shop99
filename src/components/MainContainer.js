fcf743fe98c3fd905c61c976f66f168e84051386
Raj Rai <rrai1590@gmail.com>
2022-07-05 11:46:39 +0530

    Integrated Menu conatiner

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -1,10 +1,36 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
 import HomeContainer from './HomeContainer'
+import { motion } from 'framer-motion'
+import {MdChevronLeft, MdChevronRight} from 'react-icons/md'
+import RowContainer from './RowContainer'
+import {useStateValue} from '../context/StateProvider'
+import MenuContainer from './MenuContainer'
 
 const MainContainer = () => {
+
+  const [{foodItems}, dispatch] = useStateValue()
+  const [scrollVal, setScroll] = useState(0)
+  
+  useEffect(()=> { 
+    
+  },[scrollVal])
   return (
     <div className='w-full h-auto flex flex-col items-center justify-center '>
-        <HomeContainer/>
+      <HomeContainer />
+      <section className='w-full my-6'>
+        <div className="w-full flex items-center justify-between">
+          <p className='text-2xl font-semibold capitalize text-headingColor relative before:absolute before:rounded-lg before:content before:w-32 before:h-1 before:-bottom-2 before:left-0 before:bg-gradient-to-tr from-orange-400 to-orange-600 transition-all ease-in-out duration-100 '>Our fresh & healthy Fruits</p>
+
+          <div className='hidden md:flex items-center gap-3'>
+            <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer hover:shadow-lg flex items-center justify-center'
+            onClick={()=>setScroll(-200)}><MdChevronLeft className='text-lg text-white' /></motion.div>
+            <motion.div whileTap={{ scale: 0.75 }} className='w-8 h-8 rounded-lg bg-orange-300 hover:bg-orange-500 cursor-pointer hover:shadow-lg flex items-center justify-center' onClick={()=> setScroll(200)}><MdChevronRight className='text-lg text-white' /></motion.div>
+          </div>
+        </div>
+        <RowContainer scrollVal={scrollVal} flag={true} data={foodItems?.filter(item => item.category === "icecreams")}/>
+      </section>
+
+      <MenuContainer />
     </div>
   )
 }
