bf4362d579329306b8cd29a9e787569d07aa345a
Raj Rai <rrai1590@gmail.com>
2022-07-16 23:52:51 +0530

    added checkout page

--- src/components/CreateContainer.js
+++ src/components/CreateContainer.js
@@ -137,7 +137,7 @@ const CreateContainer = () => {
   }
 
   return (
-    <div className='w-full min-h-screen h-auto flex items-center justify-center '>
+    <div className='w-full min-h-screen h-auto flex items-center justify-center mt-6'>
       <div className='w-[90%] md:w-[75%] border border-gray-300 rounded-lg p-4 flex flex-col items-center justify-center gap-4'>
         {
           fields && (
